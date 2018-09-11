4707cd00b1dc9cbefc629d14dd44a526ac6b0f99
Joey 周新华 <joey.zhou@siyue.cn>
2018-09-11 13:55:25 +0800

    个人流水时间本周改成前七天

--- src/main/java/bjl/domain/service/scoredetailed/ScoreDetailedService.java
+++ src/main/java/bjl/domain/service/scoredetailed/ScoreDetailedService.java
@@ -123,26 +123,32 @@ public class ScoreDetailedService implements IScoreDetailedService{
                 Date date=new Date();
                 Calendar calendar = Calendar.getInstance();
                 calendar.setTime(date);
-                calendar.add(Calendar.DAY_OF_MONTH, -3);
+                calendar.add(Calendar.DAY_OF_MONTH, -2);
                 dt = calendar.getTime();
 
             }
-            //表示本周
+            //表示前七天
             if(3 == command.getTimeType()){
-                Calendar cal = Calendar.getInstance();
-                cal.setTime(dt);
-                // 获得当前日期是一个星期的第几天
-                int dayWeek = cal.get(Calendar.DAY_OF_WEEK);
-                if (1 == dayWeek) {
-                    cal.add(Calendar.DAY_OF_MONTH, -1);
-                }
-                // 设置一个星期的第一天，按中国的习惯一个星期的第一天是星期一
-                cal.setFirstDayOfWeek(Calendar.MONDAY);
-                // 获得当前日期是一个星期的第几天
-                int day = cal.get(Calendar.DAY_OF_WEEK);
-                // 根据日历的规则，给当前日期减去星期几与一个星期第一天的差值
-                cal.add(Calendar.DATE, cal.getFirstDayOfWeek() - day);
-                dt = cal.getTime();
+                Date date=new Date();
+                Calendar calendar = Calendar.getInstance();
+                calendar.setTime(date);
+                calendar.add(Calendar.DAY_OF_MONTH, -6);
+                dt = calendar.getTime();
+
+//                Calendar cal = Calendar.getInstance();
+//                cal.setTime(dt);
+//                // 获得当前日期是一个星期的第几天
+//                int dayWeek = cal.get(Calendar.DAY_OF_WEEK);
+//                if (1 == dayWeek) {
+//                    cal.add(Calendar.DAY_OF_MONTH, -1);
+//                }
+//                // 设置一个星期的第一天，按中国的习惯一个星期的第一天是星期一
+//                cal.setFirstDayOfWeek(Calendar.MONDAY);
+//                // 获得当前日期是一个星期的第几天
+//                int day = cal.get(Calendar.DAY_OF_WEEK);
+//                // 根据日历的规则，给当前日期减去星期几与一个星期第一天的差值
+//                cal.add(Calendar.DATE, cal.getFirstDayOfWeek() - day);
+//                dt = cal.getTime();
 
             }
             //开始时间
